b9a292c0742f76c8103ecf6882e8977b79c62f19
Alao-Abiodun <abiodunDev@gmail.com>
2020-01-05 10:20:11 -0800

    new-changes-to-the-file

--- 02-Day/main.js
+++ 02-Day/main.js
@@ -1,1 +1,67 @@
 // this is your main.js script
+
+// Exercise Answer
+
+// Exercise 1
+let challenge = '30 Days Of JavaScript';
+// Exercise 2
+console.log(challenge);
+// Exercise 3
+console.log(challenge.length);
+// Exercise 4
+console.log(challenge.toUpperCase());
+// Exercise 5
+console.log(challenge.toLowerCase());
+// Exercise 6
+console.log(challenge.substr(0,2));
+console.log(challenge.substring(0,3));
+// Exercise 7
+console.log(challenge.substring(3,21));
+// Exercise 8
+let newChallengeString = 'You cannot end a sentence with because because because is a conjunction';
+console.log(newChallengeString.substr(31,23));
+// Exercise 9
+console.log(challenge.includes('Script'));
+// Exercise 10
+console.log(challenge.split());
+// Exercise 11
+console.log(challenge.split(' '));
+// Exercise 12 
+let techCompany = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(techCompany.split(','));
+// Exercise 13
+console.log(challenge.replace('JavaScript', 'Python'));
+// Exercise 14
+console.log(challenge.charAt(15));
+// Exercise 15
+console.log(challenge.charCodeAt('J'));
+// Exercise 16
+console.log(challenge.indexOf('a'));
+// Exercise 17
+console.log(challenge.lastIndexOf('a'));
+// Exercise 18
+console.log(newChallengeString.indexOf('because'));
+// Exercise 19
+console.log(newChallengeString.lastIndexOf('because'));
+// Exercise 20
+console.log(newChallengeString.search('because'));
+// Exercise 21
+console.log(challenge.trim());
+// Exercise 22
+console.log(challenge.startsWith('30 Days Of JavaScript'));
+// Exercise 23
+console.log(challenge.endsWith('30 Days Of JavaScript'));
+// Exercise 24
+console.log(challenge.match('a'));
+// Exercise 25
+console.log(newChallengeString.match('because'));
+// Exercise 26
+let str_1 = '30 Days of';
+let str_2 = 'JavaScript';
+console.log(str_1.concat(str_2));
+// Exercise 27
+console.log(challenge.repeat(2));
+// Exercise 31 
+console.log(`The quote '\There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help to one another.`);
+// Exercise 32
+console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`)
