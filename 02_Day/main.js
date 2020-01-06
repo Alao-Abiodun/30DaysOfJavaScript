f8fb3467ed793785e053549309359b95e2873899
Alao-Abiodun <abiodunDev@gmail.com>
2020-01-05 16:04:46 -0800

    Solve the Day2 exercise in 30 Days Of JavaScript

--- 02_Day/main.js
+++ 02_Day/main.js
@@ -87,7 +87,7 @@ console.log(Math.floor(Math.random() * 101));
 // Exercise 36
 
 // Exercise 37
-console.log(Math.floor(Math.random() * 256));
+
 // Exercise 38
 
 let randValue = challenge.substr()
