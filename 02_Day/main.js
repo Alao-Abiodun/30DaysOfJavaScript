aa380e0f1abbc6b4c0d301e3fed66fbc7751d22f
Alao-Abiodun <abiodunDev@gmail.com>
2020-01-05 16:02:58 -0800

    tackle the Day2 exercise in 30 Days Of JavaScript

--- 02_Day/main.js
+++ 02_Day/main.js
@@ -4,6 +4,7 @@
 
 // Exercise 1
 let challenge = '30 Days Of JavaScript';
+let challenge_2 = '30 Days Of Python';
 // Exercise 2
 console.log(challenge);
 // Exercise 3
@@ -52,7 +53,9 @@ console.log(challenge.startsWith('30 Days Of JavaScript'));
 // Exercise 23
 console.log(challenge.endsWith('30 Days Of JavaScript'));
 // Exercise 24
-console.log(challenge.match('a'));
+let challengePattern = '30 Days of JavaScript';
+let pattern = /a/g;
+console.log(challengePattern.match(pattern));
 // Exercise 25
 console.log(newChallengeString.match('because'));
 // Exercise 26
@@ -61,7 +64,39 @@ let str_2 = 'JavaScript';
 console.log(str_1.concat(str_2));
 // Exercise 27
 console.log(challenge.repeat(2));
+// Exercise 29
+let string = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+const [income_1, income_2, income_3] = string.match(/\d+/g);
+let totalAnnualIncome = parseInt(income_1) + parseInt(income_2) + parseInt(income_3);
+console.log(`The total annual income of the person is: ${totalAnnualIncome}`);
+// Exercise 30
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is also $the $result of &love& of tea&ching'
+
 // Exercise 31 
 console.log(`The quote '\There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help to one another.`);
 // Exercise 32
 console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`)
+// Exercise 33 
+console.log(challenge.includes('on'));
+console.log(challenge_2.includes('on'));
+// Exercise 34
+let sentence_2 = 'I hope this course is not full of jargon.';
+console.log(sentence_2.includes('jargon'));
+// Exercise 35
+console.log(Math.floor(Math.random() * 101));
+// Exercise 36
+
+// Exercise 37
+console.log(Math.floor(Math.random() * 256));
+// Exercise 38
+
+let randValue = challenge.substr()
+// Exercise 39
+console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 17\n4 1 4 16 64\n5 1 5 25 125');
+// Exercise 41
+console.log(10 == '10');
+console.log(10 === '10');
+// Exercise 42 
+console.log(parseInt(9.8));
+console.log(Math.round(9.8));
+
