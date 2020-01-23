01e51788dcca140338badba7dfab6fa94e073c18
Alao-Abiodun <abiodunDev@gmail.com>
2020-01-23 08:52:14 -0800

    attempting exercise3 of Day21:DOM 30DaysOfJavaScript challenge

--- 21_Day/Day21_Exercise.js
+++ 21_Day/Day21_Exercise.js
@@ -51,7 +51,78 @@
 // EXERCISE LEVEL 3
 
 // NO 1 MINI PROJECT 1
+// let colors = ['blue', 'yellow', 'purple', 'lightgreen', 'indigo'];
+// let numReg = /\d+/g;
+// let headingOne = document.querySelector('h1');
+// let value = headingOne.textContent;
+// let extractValue = value.substr(31, 4);
+// extractValue.style.color = 'red';
+// console.log(value);
+// console.log(extractValue);
+// let year = value.match(numReg);
+// let changedYear = parseInt(year);
+// console.log(typeof changedYear);
 
+const createDateTimeYearFormat = () => {
+   const now = new Date();
+   let month = now.getMonth() + 1;
+   switch(month){
+       case 1:
+        month = 'January';
+        break;
+       case 2:
+        month = 'Febraury';
+        break;
+       case 3:
+        month = 'March';
+        break;
+       case 4:
+        month = 'April';
+        break;
+       case 5:
+        month = 'May';
+        break;
+       case 6:
+        month = 'June';
+        break;
+       case 7:
+        month = 'July';
+        break   
+   }
+   const day = now.getUTCDate();
+   const year = now.getFullYear();
+   const hour = now.getHours();
+   const minutes = now.getMinutes();
+   const seconds = now.getSeconds();
+   let calendar = `${month} ${day}, ${year} ${hour}:${minutes}:${seconds}`;
+   console.log(calendar);
+}
+
+createDateTimeYearFormat();
+
+let allList = document.querySelectorAll('li');
+allList.forEach(list => {
+    list.style.border = '0px solid black';
+    list.style.fontSize = '20px';
+    list.style.listStyle = 'none';
+    list.style.padding = '24px';
+    list.style.marginBottom = '3px';
+    allList[0].style.backgroundColor = 'green';
+    allList[1].style.backgroundColor = 'yellow';
+    allList[2].style.backgroundColor = 'red';
+    allList[3].style.backgroundColor = 'red';
+    allList[4].style.backgroundColor = 'red';
+    allList[5].style.backgroundColor = 'red';
+    allList[6].style.backgroundColor = 'red';
+})
+
+let headingTwo = document.querySelector('h2');
+headingTwo.style.textDecoration = 'underline';
+headingTwo.style.textAlign = 'center';
+
+
+// console.log(allList);
+// console.log(headingTwo);
 
 
 
