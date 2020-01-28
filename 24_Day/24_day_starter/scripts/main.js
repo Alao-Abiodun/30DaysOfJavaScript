3a5b534f28076f53ace9c289ac206172f1a5e393
Alao-Abiodun <abiodunDev@gmail.com>
2020-01-28 07:28:59 -0800

    fixing

--- 24_Day/24_day_starter/scripts/main.js
+++ 24_Day/24_day_starter/scripts/main.js
@@ -1,2 +1,19 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+let body = document.querySelector('body');
+
+const styleHeadingOne = () => {
+    let headingOne = document.querySelector('h1');
+    headingOne.style.color = 'white' 
+    headingOne.style.textAlign = 'center'
+    // headingOne.style.fontFamily = 'Montserrat'
+}
+
+const styleFormElement = () => {
+    let massInput = document.querySelector('#mass')
+    let selectInput = document.querySelector('select')
+    let buttonCalculateWeight = document.querySelector('button')
+
+    console.log(massInput);
+}
+
+styleHeadingOne();
+styleFormElement();
--- 25_Day/25_day_starter/scripts/main.js
+++ 25_Day/25_day_starter/scripts/main.js
@@ -1,2 +1,33 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+let subtitleParagraph = document.querySelector('.subtitle');
+let feedbackParagraph = document.querySelector('.feedback');
+
+feedbackParagraph.textContent = `Currently, we have ${countries.length} countries`;
+feedbackParagraph.style.textAlign = 'center';
+feedbackParagraph.style.fontSize = '20px'
+feedbackParagraph.style.color = 'gray';
+feedbackParagraph.style.fontFamily = 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif'
+
+const styleButton = () => {
+    let graphButton = document.querySelector('.graph-buttons');
+    let buttonPopulation = document.querySelector('.population');
+    let buttonLanguage = document.querySelector('.languages');
+
+    graphButton.style.textAlign = 'center';
+
+    buttonPopulation.style.backgroundColor = 'orange';
+    buttonLanguage.style.backgroundColor = 'orange';
+    buttonPopulation.style.border = 'none';
+    buttonLanguage.style.border = 'none';
+    buttonPopulation.style.borderRadius = '2px';
+    buttonLanguage.style.borderRadius = '2px';
+    buttonPopulation.style.width = '120px'
+    buttonPopulation.style.height = '35px';
+    buttonPopulation.style.textTransform = 'uppercase';
+    buttonLanguage.style.width = '120px'
+    buttonLanguage.style.height = '35px';
+    buttonLanguage.style.textTransform = 'uppercase';
+    buttonPopulation.style.padding = '10px';
+    buttonLanguage.style.padding = '10px';
+}
+
+styleButton();
