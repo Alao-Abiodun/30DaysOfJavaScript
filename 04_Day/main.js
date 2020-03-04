59ba01fc65cbaeb6d95d0c227089819fd7878a3e
Alao-Abiodun <abiodunDev@gmail.com>
2020-03-04 20:27:15 +0100

    solve the Day04 challenge

---
+++ 04_Day/main.js
@@ -0,0 +1,30 @@
+// EXERCISE LEVEL
+// No 1
+// let age1 = prompt('Enter your age?');
+// if (2020 - parseInt(age1) >= 18) {
+//     console.log(`Enter your age: ${2020-parseInt(age1)}\nYou are old enough to drive`)
+// } else {
+//     console.log(`Enter your age: ${2020-parseInt(age1)}\nYou are left with 3 years to drive`)
+// }
+// No 2
+// let myAge = prompt('Enter my age?');
+// let yourAge = prompt('Enter your age?');
+// if (parseInt(myAge) < parseInt(yourAge)) {
+//     console.log(`Enter your age: ${yourAge}\nYou are ${yourAge - myAge} years older than me.`)
+// }
+// No 3
+// let a = 4;
+// let b = 3;
+// if (a > b) {
+//     console.log(`${a} is greater than ${b}`)
+// }
+// a  > b ?
+// console.log(`${a} is greater than ${b}`) :
+// console.log(`your condition is not correct`)
+// No 4
+let numbers = prompt('Enter any number')
+if (parseInt(numbers) % 2 == 0) {
+    console.log(`Enter a number:  ${numbers}\n${numbers} is an even number.`)
+} else {
+    console.log(`Enter a number:  ${numbers}\n${numbers} is an odd number.`)
+}
