6bd5f8f244b924b7821f9c0d8d3963212dd29607
Alao-Abiodun <abiodunDev@gmail.com>
2020-03-05 18:51:59 +0100

    cleaning the Day05 files

--- 05_Day/Day05_Exercise.js
+++
@@ -1,4 +0,0 @@
-const iTCompanies = ['Facebook', 'Google', 'Microsoft','Apple', 'IBM', 'Oracle','Amazon'];
-const techCompanies = iTCompanies.slice(0, iTCompanies.length-1).join(', ');
-const companies = iTCompanies.slice(iTCompanies.length-1);
-console.log(`${techCompanies} and ${companies} are big IT companies`);
