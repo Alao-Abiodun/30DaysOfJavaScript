44124279d0df09214d0fdcc710ce59a2bc773f72
Abiodun <abiodundev@gmail.com>
2020-12-24 03:27:10 +0100

    chore/ solve day Mini project of Day 21 of 30DaysOfJavaScript

--- 21_Day/main.js
+++ 21_Day/main.js
@@ -103,10 +103,62 @@ let changeYearColor = () => {
 };
 setInterval(changeYearColor, 2000);
 
-console.log(year);
+let headingTwo = document.querySelector('h2');
+headingTwo.style.textDecoration = 'underline';
+let div = document.createElement('div');
+div.style.fontSize = '20px';
+div.style.width = '300px';
+div.style.marginTop = '5px';
+div.style.border = '1px solid black';
+div.style.display = 'flex';
+div.style.flexDirection = 'column';
+div.style.justifyContent = 'center';
+div.style.alignItems = 'center';
+div.style.height = '40px';
+let mydate = new Date();
+let month = mydate.getMonth() + 1;
+let day = mydate.getDate();
+let newyear = mydate.getFullYear();
+let hours = mydate.getHours();
+let minutes = mydate.getMinutes();
+let seconds = mydate.getSeconds();
+let fullCalendarDate = `${month} ${day}, ${newyear} ${hours}:${minutes}:${seconds}`;
+div.innerText = fullCalendarDate;
+
+headingTwo.appendChild(div);
+
+let createDateContainerBackground = () => {
+  let randomColor = Math.floor(Math.random() * 16777216).toString(16);
+  div.style.backgroundColor = `#${randomColor}`;
+};
+setInterval(createDateContainerBackground, 2000);
+
+let challengeList = () => {
+  let allChanllenge = document.querySelectorAll('li');
+  let challengeArray = [...allChanllenge];
+  challengeArray.map(element => {
+    element.style.listStyle = 'none';
+    element.style.width = '700px';
+    element.style.height = '50px';
+    element.style.marginTop = '5px';
+    element.style.display = 'flex';
+    element.style.flexDirection = 'column';
+    element.style.justifyContent = 'center';
+    element.style.paddingLeft = '10px';
+    element.style.alignItems = 'flex-start';
+    if (element.textContent.includes('Done')) {
+      element.style.backgroundColor = 'Green';
+    } else if (element.textContent.includes('Ongoing')) {
+      element.style.backgroundColor = 'Yellow';
+    } else {
+      element.style.backgroundColor = 'red';
+    }
+  });
+};
+
+challengeList();
+
 wrapper.style.display = 'flex';
 wrapper.style.flexDirection = 'column';
 wrapper.style.justifyContent = 'center';
 wrapper.style.alignItems = 'center';
-
-console.log(wrapper);
